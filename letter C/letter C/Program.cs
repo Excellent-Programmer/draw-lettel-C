51dca3084daae201664734c4bf9dae740e507968
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-21 15:12:55 +0200

    Add files via upload

---
+++ letter C/letter C/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class letter_C
+{
+    public static void Main()
+    {
+        int row, column;
+
+        for (row = 0; row <= 6; row++)
+        {
+            for (column = 0; column <= 6; column++)
+            {
+                if ((column == 1 && (row != 0 && row != 6)) || ((row == 0 || row == 6) && (column > 1 && column < 5)) || (column == 5 && (row == 1 || row == 5)))
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+        Console.Write("\n");
+        Console.Read();
+    }
+}
